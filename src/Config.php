5d4dbb3fe0d0d658b8389743cacbc636ed304036
Joseph Gabito <joseph@uncannyowl.com>
2022-07-31 16:16:33 +0800

    Update Config.php

--- src/Config.php
+++ src/Config.php
@@ -1,11 +1,11 @@
 <?php
+
 namespace LogEntriesEtc;
 
-class Config {
-
-
-	public function getBaseURL() {
-		return plugin_dir_url( __FILE__ );
-	}
-
+class Config
+{
+    public function getBaseURL()
+    {
+        return plugin_dir_url(__FILE__);
+    }
 }

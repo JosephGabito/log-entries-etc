071a46fa15800e34b9085a1914164fdbb009213f
Joseph Gabito <joseph@uncannyowl.com>
2022-07-31 17:23:04 +0800

    Update HTTPRequestCollector.php

--- src/Collectors/HTTPRequestCollector.php
+++ src/Collectors/HTTPRequestCollector.php
@@ -13,14 +13,16 @@ class HTTPRequestCollector
 
     public function dispatch()
     {
-
         add_action('http_api_debug', array( $this, 'collect' ), 10, 5);
-        add_filter('pre_http_request', array( $this, 'startProfile' ), 10, 2);
+        add_filter('pre_http_request', array( $this, 'startProfile' ), 10, 3);
     }
 
-    public function startProfile($parsed_args, $url)
+    public function startProfile($preempt, $parsed_args, $url)
     {
-        $this->debugbar['time']->startMeasure('http_requests', 'HTTP Requests');
+		$domain = str_ireplace('www.', '', parse_url($url, PHP_URL_HOST));
+        $this->debugbar['time']->startMeasure('http_requests', $domain);
+
+		return $preempt;
     }
 
     public function collect($response, $requests, $class, $parsed_args, $url)
@@ -30,27 +32,15 @@ class HTTPRequestCollector
             $this->debugbar->addCollector(new \DebugBar\DataCollector\MessagesCollector('HTTP_Requests'));
         }
 
-        $status_code = wp_remote_retrieve_response_code($response);
-
-        if (! in_array($status_code, self::HTTP_OK_STATUSES, true)) {
-            $this->debugbar['HTTP_Requests']->error($url);
-            $this->debugbar['HTTP_Requests']->error('Status code: ' . $status_code);
-        } else {
-            $this->debugbar['HTTP_Requests']->info($url);
-            $this->debugbar['HTTP_Requests']->info('Status code: ' . $status_code);
-        }
+       	$this->collectStatusCodes( $response, $url );
 
         $this->debugbar['HTTP_Requests']->info(
             array(
                 'Request' => $parsed_args,
             )
         );
-
-        $this->debugbar['HTTP_Requests']->info(
-            array(
-                'Response:Body' => wp_remote_retrieve_body($response),
-            )
-        );
+		
+		$this->collectResponseBody( $response );
 
         $this->debugbar['HTTP_Requests']->info(
             array(
@@ -73,4 +63,43 @@ class HTTPRequestCollector
         $this->debugbar['HTTP_Requests']->info('End~');
         $this->debugbar['time']->stopMeasure('http_requests');
     }
+
+	public function collectStatusCodes( $response, $url ) {
+
+		$status_code = wp_remote_retrieve_response_code($response);
+
+		if ( empty( $status_code ) ) {
+			$this->debugbar['HTTP_Requests']->info($url);
+			$this->debugbar['HTTP_Requests']->error('Status code empty. Possible a non-blocking request.');
+			return;
+		}
+
+		if (! in_array($status_code, self::HTTP_OK_STATUSES, true)) {
+            $this->debugbar['HTTP_Requests']->error($url);
+            $this->debugbar['HTTP_Requests']->error('Status code: ' . $status_code);
+			return;
+        }
+
+		$this->debugbar['HTTP_Requests']->info($url);
+		$this->debugbar['HTTP_Requests']->info('Status code: ' . $status_code);
+        
+		return $this;
+		
+	}
+
+	public function collectResponseBody( $response ) {
+
+		$status_code = wp_remote_retrieve_response_code($response);
+
+		// Only add response body if status code is not empty.
+		if ( ! empty( $status_code ) ) {
+			$this->debugbar['HTTP_Requests']->info(
+				array(
+					'Response:Body' => wp_remote_retrieve_body($response),
+				)
+			);
+		}
+
+		return $this;
+	}
 }
